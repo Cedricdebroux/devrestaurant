d904eab29e799e13f47d748a08394e247b0bce71
Emi <pieront.emilie@gmail.com>
2021-01-14 11:46:41 +0100

    imagesGallery

--- header.php
+++ header.php
@@ -4,8 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <?php wp_head(); ?>
     <script defer src="assets/js/footer.js"></script>
+    <?php wp_head(); ?>
+    
+    
   </head>
   <body <?php body_class(); ?>>
 
