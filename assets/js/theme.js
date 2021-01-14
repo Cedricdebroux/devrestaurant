2a5a098c0cf1edd27d856702c2cfa126392957ec
Emi <pieront.emilie@gmail.com>
2021-01-14 12:02:34 +0100

    imagesGallery

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,3 +2,29 @@
   var projectButton = document.querySelector( '.single-portfolio a.button' );
   projectButton.target = '_blank';
 })();
+
+
+
+// FOOTER PARTS 
+
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
