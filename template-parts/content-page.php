4814281cae72f932bd2ede15448ea678b919585f
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-01-15 14:06:56 +0100

    acf menu title

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -8,11 +8,17 @@
 
   <div class="entry-content">
     <div class="theMenu_welcome">
-
+    <?php 
+      if(have_rows('category1')) :
+        while(have_rows('category1')) : the_row();
+          the_sub_field('category_title');
+        endwhile;  
+      endif;  
+    ?>
     </div>
 
     <?php the_content(); ?>
-  
+    <p>Template:content page.php</p>
 
     <?php 
       if(have_rows('menu')) :
