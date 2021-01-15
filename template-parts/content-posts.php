96bcefbd9a0db6792ff03b1b63bd38bef31ad525
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-01-15 10:16:59 +0100

    test menu 2

--- template-parts/content-posts.php
+++ template-parts/content-posts.php
@@ -13,16 +13,15 @@
   </header>
 
   <div class="entry-content">
-  <div class="the_menu_title">
-  <?php the_field('the_menu_title'); ?>
-  <?php the_field('title'); ?>
-  <?php the_field('text'); ?>
-  <?php the_field('price'); ?>
-  </div>
 
     <?php the_excerpt(); ?>
 
   </div>
-
+  <div class="the_menu_title">
+<?php the_field('the_menu_title'); ?>
+<?php the_field('title'); ?>
+<?php the_field('text'); ?>
+<?php the_field('price'); ?>
+</div>
 
 </article>
