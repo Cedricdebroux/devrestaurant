8f20ab0c5faa7f1f7afaca98aada54000a86d71c
Emi <pieront.emilie@gmail.com>
2021-01-16 18:43:45 +0100

    footer gallery

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,7 +2,7 @@ const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
-const images = document.querySelectorAll('footer instaPictures .insta')
+const images = document.querySelectorAll('.insta')
 images.forEach(image => {
   image.addEventListener('click', e => {
     lightbox.classList.add('active')
