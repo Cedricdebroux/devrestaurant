547d14c48529b988561246db3f77dd4d27aca495
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-01-16 13:23:13 +0100

    acf menu

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -32,6 +32,50 @@
       endif;  
     ?>
     </div>
+    <p class="h1 mt-3 d-flex justify-content-center theMenu_category">
+    <?php 
+      if(have_rows('category2')) :
+        while(have_rows('category2')) : the_row();
+          the_sub_field('category_title');
+        endwhile;  
+      endif;  
+    ?>
+</p>
+    <div class="menu2">
+<?php 
+  if(have_rows('menu2')) :
+    while(have_rows('menu2')) : the_row(); ?>
+        <div class="container row row-cols-3">
+            <div class="col"> <?php the_sub_field('name_of_the_meal');?> </div>
+            <div class="col"> <?php the_sub_field('description_of_the_meal');?> </div>
+            <div class="col"> <?php the_sub_field('price_of_the_meal');?> </div>
+        </div>
+   <?php endwhile;  
+  endif;  
+?>
+</div>
+<p class="h1 mt-3 d-flex justify-content-center theMenu_category">
+    <?php 
+      if(have_rows('category3')) :
+        while(have_rows('category3')) : the_row();
+          the_sub_field('category_title');
+        endwhile;  
+      endif;  
+    ?>
+</p>
+<div class="menu3">
+<?php 
+  if(have_rows('menu3')) :
+    while(have_rows('menu3')) : the_row(); ?>
+        <div class="container row row-cols-3">
+            <div class="col"> <?php the_sub_field('name_of_the_meal');?> </div>
+            <div class="col"> <?php the_sub_field('description_of_the_meal');?> </div>
+            <div class="col"> <?php the_sub_field('price_of_the_meal');?> </div>
+        </div>
+   <?php endwhile;  
+  endif;  
+?>
+</div>
 
   </div>
 
