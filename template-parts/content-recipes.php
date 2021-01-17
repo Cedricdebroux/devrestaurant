c7be61ca26564a7f42b9d8b81969818eee7dc942
User <rachida.haddouch.webdev@gmail.com>
2021-01-17 20:12:09 +0100

    single recipe

--- template-parts/content-recipes.php
+++ template-parts/content-recipes.php
@@ -2,18 +2,30 @@
 
   <header class="entry-header">
 
-    <?php the_title( '<h2><a href="' . get_the_permalink() .'">', '</a></h2>' ); ?>
+  
 
-    <a href="<?php the_permalink(); ?>">
-      <?php the_post_thumbnail( 'full' ); ?>
-    </a>
+   
 
   </header>
 
-  <div class="entry-content">
+  <div class="entry-content container">
 
-    <?php the_excerpt(); ?>
-    <?php the_field('description');?>
+  <div class="card mb-12 text-center">
+  <div class="row no-gutters">
+    <div class="col-md-7">
+    <?php the_post_thumbnail( 'full' ); ?>
+
+    </div>
+    <div class="col-md-5">
+      <div class="card-body">
+      <p class="card-text"><small class="text-muted"><?php echo get_the_date( 'j F' ); ?></small></p>
+        <h5 class="card-title"> <?php the_title( '<h2><a href="' . get_the_permalink() .'">', '</a></h2>' ); ?></h5>
+        <p class="card-text"> <?php the_field('description');?></p>
+      <button type="button" class="btn btn-dark"> <a href="<?php the_permalink(); ?>">Read more </a></button> 
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
