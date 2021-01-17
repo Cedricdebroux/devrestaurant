0f37a91537aeb5aa88caec0d6f7e9c8f53d64138
User <rachida.haddouch.webdev@gmail.com>
2021-01-17 19:53:50 +0100

    single recipe

--- template-parts/content-recipes.php
+++ template-parts/content-recipes.php
@@ -1,37 +1,29 @@
 <article id="post-<?php the_ID(); ?>"  <?php post_class(); ?>>
 
-  
+  <header class="entry-header">
+
+    <?php the_title( '<h2><a href="' . get_the_permalink() .'">', '</a></h2>' ); ?>
+
+    <a href="<?php the_permalink(); ?>">
+      <?php the_post_thumbnail( 'full' ); ?>
+    </a>
+
+  </header>
 
   <div class="entry-content">
 
     <?php the_excerpt(); ?>
-   
+    <?php the_field('description');?>
 
 
 
 
-    <div class="card mb-3">
-  <div class="row no-gutters">
-    <div class="col-md-7">
-    <a href="<?php the_permalink(); ?>">
-    <?php the_post_thumbnail( 'full' ); ?>
-    </a>
-    </div>
-    <div class="col-md-5">
-      <div class="card-body">
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        <h5 class="card-title"> <?php the_title( '<h2><a href="' . get_the_permalink() .'">', '</a></h2>' ); ?></h5>
-        <p class="card-text"> <?php the_field('description');?></p>
-       
-      </div>
-    </div>
-  </div>
-</div>
+    
 
 
 
 
-
+    
 
   </div>
 
