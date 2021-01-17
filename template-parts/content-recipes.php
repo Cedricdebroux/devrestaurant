f641655282b5f1668fd5b50c3fd47f8cf6d47b82
User <rachida.haddouch.webdev@gmail.com>
2021-01-17 19:35:55 +0100

    single recipe

--- template-parts/content-recipes.php
+++ template-parts/content-recipes.php
@@ -13,8 +13,31 @@
   <div class="entry-content">
 
     <?php the_excerpt(); ?>
-    <?php the_field('description');?>
-    
+   
+
+
+
+
+    <div class="card mb-3" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-7">
+    <?php the_post_thumbnail( 'full' ); ?>
+    </div>
+    <div class="col-md-5">
+      <div class="card-body">
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <h5 class="card-title"> <?php the_title( '<h2><a href="' . get_the_permalink() .'">', '</a></h2>' ); ?></h5>
+        <p class="card-text"> <?php the_field('description');?></p>
+       
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
 
   </div>
 
