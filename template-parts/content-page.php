ae63f83e47a2c5a05fe33933b423968d50dd3889
Emi <pieront.emilie@gmail.com>
2021-01-17 18:13:08 +0100

    menu

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -29,13 +29,13 @@
     <?php 
       if(have_rows('menu1')) :
         while(have_rows('menu1')) : the_row(); ?>
-            <div class="container row row-cols-3">
-              <div class="col"><p class="menuTitle"><?php the_sub_field('name_of_the_meal');?></p></div>
-              <div class="col"><hr class="hr1"></div>
-              <div class="col"><?php the_sub_field('price_of_the_meal');?> </div>
-              <div class="mealDescription">
-                <?php the_sub_field('description_of_the_meal');?>
-              </div>
+            <div class="gridMenu">
+              <div><p class="menuTitle"><?php the_sub_field('name_of_the_meal');?></p></div>
+              <div><hr class="hr1"></div>
+              <div><?php the_sub_field('price_of_the_meal');?> </div>
+            </div>
+            <div class="mealDescription">
+              <?php the_sub_field('description_of_the_meal');?>
             </div>
        <?php endwhile;  
       endif;  
