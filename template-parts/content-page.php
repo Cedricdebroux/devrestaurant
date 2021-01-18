b0dd489c44876d8f7a5bc120c6a6041b08da8b72
Emi <pieront.emilie@gmail.com>
2021-01-18 10:56:54 +0100

    menu

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -29,7 +29,7 @@
     <?php 
       if(have_rows('menu1')) :
         while(have_rows('menu1')) : the_row(); 
-          if(the_sub_field('chef_selection') == "Yes") :?>
+          if(the_sub_field('chef_selection')) :?>
             <div class="chefSelection">
               <div class="gridMenu">
                 <div><p class="menuTitle"><?php the_sub_field('name_of_the_meal');?></p></div>
