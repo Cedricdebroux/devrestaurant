638a5e20eb408036daa80a09106500d87a91506a
Emi <pieront.emilie@gmail.com>
2021-01-18 13:40:33 +0100

    menu

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -100,50 +100,50 @@
     endwhile; 
   endif;  
     ?>
-  </div>
-<p class="h2 mt-3 d-flex justify-content-center theMenu_category">
-  <?php 
-    if(have_rows('category3')) :
-      while(have_rows('category3')) : the_row();
-        the_sub_field('category_title');
-      endwhile;  
-    endif;  
-  ?>
-</p>
-<div class="menu3">
-  <?php 
-    if(have_rows('menu3')) :
-      while(have_rows('menu3')) : the_row(); 
-        $sub_value = get_sub_field('chef_selection'); 
-        if( $sub_value == 1) :?>
-          <div class="chefBox">CHEF SELECTION</div>
-          <div class="chefSelection">
+    </div>
+    <p class="h2 mt-3 d-flex justify-content-center theMenu_category">
+      <?php 
+        if(have_rows('category3')) :
+          while(have_rows('category3')) : the_row();
+            the_sub_field('category_title');
+          endwhile;  
+        endif;  
+      ?>
+    </p>
+    <div class="menu3">
+      <?php 
+        if(have_rows('menu3')) :
+          while(have_rows('menu3')) : the_row(); 
+            $sub_value = get_sub_field('chef_selection'); 
+            if( $sub_value == 1) :?>
+              <div class="chefBox">CHEF SELECTION</div>
+              <div class="chefSelection">
+                <div class="gridMenu">
+                  <div><p class="menuTitle"><?php the_sub_field('name_of_the_meal');?></p></div>
+                  <div><hr class="hr1 mr-2"></div>
+                  <div class="menuPrice"><?php the_sub_field('price_of_the_meal');?>€ </div>
+                </div>
+                <div class="mealDescription">
+                  <?php the_sub_field('description_of_the_meal');?>
+                </div>
+              </div>
+
+                <?php 
+            else : ?>
             <div class="gridMenu">
               <div><p class="menuTitle"><?php the_sub_field('name_of_the_meal');?></p></div>
-              <div><hr class="hr1 mr-2"></div>
+              <div><hr class="hr1 mr-2 "></div>
               <div class="menuPrice"><?php the_sub_field('price_of_the_meal');?>€ </div>
             </div>
             <div class="mealDescription">
               <?php the_sub_field('description_of_the_meal');?>
             </div>
-          </div>
-
-            <?php 
-        else : ?>
-        <div class="gridMenu">
-          <div><p class="menuTitle"><?php the_sub_field('name_of_the_meal');?></p></div>
-          <div><hr class="hr1 mr-2 "></div>
-          <div class="menuPrice"><?php the_sub_field('price_of_the_meal');?>€ </div>
-        </div>
-        <div class="mealDescription">
-          <?php the_sub_field('description_of_the_meal');?>
-        </div>
-          
-      <?php endif;
-      endwhile;  
-    endif;  
-  ?>
-</div>
+              
+          <?php endif;
+          endwhile;  
+        endif;  
+      ?>
+    </div>
 </div>
   </div>
 </article>
