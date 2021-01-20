b822aeb024619f14b09b452f8321bdd1448a6e6d
Emi <pieront.emilie@gmail.com>
2021-01-20 10:16:22 +0100

    single recipe blog

--- archive-recipes-post.php
+++ archive-recipes-post.php
@@ -6,7 +6,7 @@
 
     
 
-      <?php $i = 1;
+      <?php $i = 2;
       if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
         <?php get_template_part( 'template-parts/content', 'recipes' ); ?>
