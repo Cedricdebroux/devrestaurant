57f1e8d25cb3998338695d9260862e15e0002ff5
Emi <pieront.emilie@gmail.com>
2021-01-20 16:20:05 +0100

    single recipe page blog

--- single-recipes-post.php
+++ single-recipes-post.php
@@ -5,25 +5,33 @@
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
           <header class="entry-header">
-            <!-- Back, date, taxonomy -->
+            <div class="recipeSinglePageHeader"> <!-- Back, date, taxonomy V -->
+              <a href="https://teamperfect.bout-de-creations.com/recipes-post/"><img src="https://teamperfect.bout-de-creations.com/wp-content/uploads/2021/01/left-arrow.svg" alt=""> <p>Retour</p></a> 
+              <?php get_the_date( 'j F, Y' ); ?>
+              <?php the_field('type_of_recipe'); ?>
+            </div>
+
             <?php the_title( '<h1>', '</h1>' ); ?>
-            <!-- Description -->
+            <!-- Description V-->
+            <?php the_field('description', 'recipe') ; ?>
+
           </header>
           <div class="entry-content">
+            <div class="recipeSinglePageContent">
+              <a href="<?php the_permalink(); ?>">
+                <?php the_post_thumbnail( 'full' ); ?> <!-- Image of the recipe V -->
+              </a>
 
-            <a href="<?php the_permalink(); ?>">
-              <?php the_post_thumbnail( 'full' ); ?> <!-- Image of the recipe -->
-            </a>
+              <?php the_content(); ?> <!-- Content of the page "Recipe" V -->
 
-            <?php the_content(); ?> <!-- Content of the page "Recipe" -->
+              <!-- ingredients -->
+              <!-- serve and preparation time --> 
+              <!-- intructions (steps) --> <!-- Aside social network -->
+              <!-- Latest blog -->
 
-            <!-- ingredients -->
-            <!-- serve and preparation time --> 
-            <!-- intructions (steps) --> <!-- Aside social network -->
-            <!-- Latest blog -->
-
-           
-            <!-- Content of the page "Recipe" -->
+            
+              <!-- Content of the page "Recipe" -->
+            </div>
           </div>
         </article>
       <?php endwhile; endif; ?>
