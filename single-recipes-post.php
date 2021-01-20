a1520be35a92076b6b6e789fa443cefd4cece5d1
Emi <pieront.emilie@gmail.com>
2021-01-20 17:02:32 +0100

    single recipe page blog

--- single-recipes-post.php
+++ single-recipes-post.php
@@ -6,7 +6,7 @@
           <header class="entry-header">
             <div class="recipeSinglePageHeader"> <!-- Back, date, taxonomy V -->
               <a href="https://teamperfect.bout-de-creations.com/recipes-post/"><img class="recipeSinglePageHeaderArrow" src="https://teamperfect.bout-de-creations.com/wp-content/uploads/2021/01/left-arrow.svg" alt=""> <p>Retour</p></a> 
-              <?php echo get_the_date( 'j F, Y' ); ?>
+              <p class="card-text"><small class="text-muted"><?php echo get_the_date( 'j F, Y' ); ?></small></p>
               <?php the_category('type_of_recipe'); ?>
             </div>
             <?php the_title( '<h1>', '</h1>' ); ?>
