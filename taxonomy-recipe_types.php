16fa8694ba8a61e0693bfdaa38697f276ae8cf56
Emi <pieront.emilie@gmail.com>
2021-01-21 16:30:40 +0100

    recipe types

--- taxonomy-recipe_types.php
+++ taxonomy-recipe_types.php
@@ -4,8 +4,6 @@
 
     <main id="main" class="site-main" role="main">
 
-      <h1><?php the_archive_title(); ?></h1>
-
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
         <?php get_template_part( 'template-parts/content', 'recipes' ); ?>
