8a623109bf9da8c5c450601d73e5b87696d70736
Emi <pieront.emilie@gmail.com>
2021-01-21 16:54:01 +0100

    recipe types CSS

--- template-parts/content-recipes.php
+++ template-parts/content-recipes.php
@@ -12,7 +12,7 @@
             <div class="col-md-5">
               <div class="card-body">
                 <p class="card-text"><small class="text-muted"><?php echo get_the_date( 'j F, Y' ); ?></small></p>
-                <h5 class="card-title"> <?php the_title( '<h2><a href="' . get_the_permalink() .'">', '</a></h2>' ); ?></h5>
+                <h5 class="card-title"> <?php the_title( '<h2><a class="text-dark" href="' . get_the_permalink() .'">', '</a></h2>' ); ?></h5>
                 <p class="card-text"> <?php the_field('description');?></p>
                 <button type="button" class="btn btn-dark text-white"> <a href="<?php the_permalink(); ?>">Read more </a></button> 
               </div>
