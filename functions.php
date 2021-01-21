30b288b628799363beb81de614e86f88919b7283
User <rachida.haddouch.webdev@gmail.com>
2021-01-21 08:39:09 +0100

    remove gutenberg for acf

--- functions.php
+++ functions.php
@@ -156,6 +156,9 @@ function register_widget_areas() {
  }
 
 
-
+  // for posts
+  add_filter('use_block_editor_for_post', '__return_false', 10);
+  // for post types
+  add_filter('use_block_editor_for_post_type', '__return_false', 10);
 
 ?>
