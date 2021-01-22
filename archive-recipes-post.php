cebb62617dcd82ace7be009272149b8b44e38ffa
Emi <pieront.emilie@gmail.com>
2021-01-22 10:05:39 +0100

    content-recipe

--- archive-recipes-post.php
+++ archive-recipes-post.php
@@ -17,7 +17,31 @@
 
       <?php endif; ?>
 
-      <?php echo paginate_links(); ?>
+      <div class="paginateArchiveBlog">
+        <?php echo paginate_links(); ?>
+      </div>
+      
+      <?php
+
+        $big = 999999999;
+
+        $paginate_links = paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $the_query->max_num_pages,
+            'prev_text'    => __('<div class="prev pull-left"><i class="icon-arrow_thin_left"></i></div>'),
+            'next_text'    => __('<div class="next pull-right"><i class="icon-arrow_thin_right"></i></div>'),
+            'type' => 'array',
+            'n_display' => 'test'
+        ) );
+
+        foreach ( $paginate_links as $pgl ) {
+            echo "$pgl";
+        }
+
+      ?>
+
 
     </main>
 
