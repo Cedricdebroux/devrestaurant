d0470e572cb9c66369c3b9b21d97ffaf30509ec3
Emi <pieront.emilie@gmail.com>
2021-01-22 17:12:00 +0100

    test

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
   <div id="primary" class="content-area extended">
 
     <main id="main" class="site-main container" role="main">
-
+            COUCOU ! 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
         <?php get_template_part( 'template-parts/content', 'page' ); ?>
