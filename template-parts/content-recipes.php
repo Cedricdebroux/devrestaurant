a67b6b5f648f739a19a32a50fc9f508d0648e9ba
User <rachida.haddouch.webdev@gmail.com>
2021-01-22 08:12:40 +0100

    taxonomy archive recipe color

--- template-parts/content-recipes.php
+++ template-parts/content-recipes.php
@@ -1,7 +1,7 @@
 <div> <!-- Fin dans le footer -->
  <?php global $wp_query; ?>
+ <p class="text-center text-black-50"><?php the_terms( $post->ID, 'recipe_types', ' ' , ' ' ); ?> </p>
   <article class="recipeContent mb-3 mt-5" id="post-<?php the_ID(); ?>"  <?php post_class(); ?>>
-  <p class="text-center text-black-50"><?php the_terms( $post->ID, 'recipe_types', ' ' , ' ' ); ?> </p>
     <div class="entry-content container">
       <!-- NOTRE SUPER BOUCLE ICI -->
       <?php if ( $wp_query->$current_post %2==0 ){ ?> 
