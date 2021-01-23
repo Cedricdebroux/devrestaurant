5a33899cf4c508cd2e7db6963fd875e323b58cdb
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-01-23 15:11:19 +0100

    test shortcode

--- single-recipes-post.php
+++ single-recipes-post.php
@@ -61,5 +61,5 @@
       <?php endwhile; endif; ?>
     </main>
   </div>
-  <?php echo do_shortcode("[hfe_template id='1557']"); ?>
+  <div><?php echo do_shortcode("[hfe_template id='1557']"); ?></div>
 <?php get_footer(); ?>
