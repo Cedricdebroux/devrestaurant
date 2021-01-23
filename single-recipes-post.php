57f432e2afc32021fd4a62a3ce7d1458c28e5f20
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-01-23 20:17:00 +0100

    single recipe background lt post cut

--- single-recipes-post.php
+++ single-recipes-post.php
@@ -61,7 +61,7 @@
       <?php endwhile; endif; ?>
     </main>
   </div>
-  <div class="recipeSingleLatestPostcut"><img src="https://teamperfect.bout-de-creations.com/wp-content/uploads/2021/01/hachures-blanches-1.png" alt=""></div>
+  <div class="recipeSingleLatestPostCut"><img src="https://teamperfect.bout-de-creations.com/wp-content/uploads/2021/01/hachures-blanches-1.png" alt=""></div>
   <div class="recipeSingleLatestPost"><?php echo do_shortcode("[hfe_template id='1557']"); ?></div>
 <div class="recipeSingleLatestPostfooterBg"></div>
   <?php get_footer(); ?>
